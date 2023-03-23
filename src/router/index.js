7b80396ca7b962038495d1cfb2458a750544bc19
José Victor <josevictordev@gmail.com>
2023-03-23 17:29:55 -0300

    feat: Route /register

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Dashboard from '../views/Dashboard.vue'
+import Register from '../views/Register.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/dashboard',
       name: 'dashboard',
       component: Dashboard
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
     }
   ]
 })
