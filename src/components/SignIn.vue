73cdec9899d13ed426270e6044bf5b8461cab679
José Victor <josevictordev@gmail.com>
2023-03-23 16:03:51 -0300

    feat: SignIn component and new spaces

---
+++ src/components/SignIn.vue
@@ -0,0 +1,74 @@
+<script>
+import { RouterLink } from 'vue-router'
+import Field from '../components/Field.vue'
+import Submit from '../components/Submit.vue'
+
+export default {
+  name: 'SignIn',
+  components: {
+    Field,
+    Submit,
+    RouterLink
+  },
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    handleSubmit() {
+      console.log(this.email)
+      console.log(this.password)
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="sign-in">
+    <img class="sign-in__logo" src="/src/assets/images/logo.svg" />
+
+    <h2 class="sign-in__title">Entre na plataforma</h2>
+
+    <form class="sign-in__form" @submit.prevent="handleSubmit">
+      <Field v-model="email" label="Email, CPF ou PIS" />
+      <Field v-model="password" label="Senha" />
+      <Submit>ENTRAR</Submit>
+    </form>
+
+    <footer class="sign-in__footer">
+      <span class="sign-in__link"
+        >Não possui conta? <RouterLink to="register">Clique aqui!</RouterLink></span
+      >
+    </footer>
+  </div>
+</template>
+<style lang="scss" scoped>
+.sign-in {
+  width: 100%;
+  max-width: 400px;
+  background-color: var(--light);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: var(--g-24);
+  border-radius: 8px;
+  padding: var(--p-32);
+
+  &__title {
+    color: var(--gray-600);
+  }
+
+  &__form {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: var(--g-24);
+  }
+  &__link {
+    color: var(--gray-600);
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,30 @@
 <script>
-'Home'
+import SignIn from '../components/SignIn.vue'
+
+export default {
+  components: { SignIn },
+  name: 'Home'
+}
 </script>
 
 <template>
-  <h1>Olá, visitante!</h1>
+  <div class="home">
+    <header class="home__header">Olá, visitante!</header>
+    <SignIn />
+  </div>
 </template>
 
 <style lang="scss" scoped>
-h1 {
-  font-size: var(--fs-32);
+.home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: var(--g-32);
+
+  &__header {
+    color: var(--light);
+    font-size: var(--fs-32);
+    text-shadow: var(--text-shadow);
+  }
 }
 </style>
