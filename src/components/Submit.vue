9cba4d94c7aef83f5185686264cf8d46b20304b2
José Victor <josevictordev@gmail.com>
2023-03-23 16:01:32 -0300

    feat: Submit component

---
+++ src/components/Submit.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: 'Submit'
+}
+</script>
+
+<template>
+  <button class="submit"><slot /></button>
+</template>
+
+<style lang="scss" scoped>
+.submit {
+  width: fit-content;
+  background-color: var(--primary-main);
+  color: var(--light);
+  font-size: var(--fs-20);
+  border: none;
+  border-radius: 8px;
+  padding: var(--p-12) var(--p-24);
+}
+</style>
