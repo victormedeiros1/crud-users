0788b64197aa22ca8970c4bebbca102fdef12096
José Victor <josevictordev@gmail.com>
2023-03-23 22:10:06 -0300

    feat: Update view

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Dashboard from '../views/Dashboard.vue'
 import Register from '../views/Register.vue'
+import Update from '../views/Update.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: Register
+    },
+    {
+      path: '/dashboard/update',
+      name: 'update',
+      component: Update
     }
   ]
 })
---
+++ src/views/Update.vue
@@ -0,0 +1,11 @@
+<script>
+import UpdateForm from '@/components/Forms/UpdateForm.vue'
+
+export default {
+  components: { UpdateForm }
+}
+</script>
+
+<template>
+  <UpdateForm />
+</template>
