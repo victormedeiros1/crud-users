5c0db7e8fa77491796b0477ceac4ee979daaaa95
José Victor <josevictordev@gmail.com>
2023-03-23 22:09:08 -0300

    feat: Prop color to 3 differente buttons

--- src/components/Submit.vue
+++ src/components/Submit.vue
@@ -1,21 +1,32 @@
 <script>
 export default {
-  name: 'Submit'
+  name: 'Submit',
+  props: {
+    color: { type: String, default: 'primary' }
+  }
 }
 </script>
 
 <template>
-  <button class="submit"><slot /></button>
+  <button :class="['submit', color]"><slot /></button>
 </template>
 
 <style lang="scss" scoped>
 .submit {
   width: fit-content;
-  background-color: var(--primary-main);
   color: var(--light);
   font-size: var(--fs-20);
   border: none;
   border-radius: 8px;
   padding: var(--p-12) var(--p-24);
+}
+.primary {
+  background-color: var(--primary-main);
+}
+.success {
+  background-color: var(--success);
+}
+.error {
+  background-color: var(--error);
 }
 </style>
